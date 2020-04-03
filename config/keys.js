5b9f525f8fd5afa4a2a6c8ccd17475f613785853
dsaimachoudhury@gmail.com <dsaimachoudhury@gmail.com>
2020-04-03 11:45:04 +0600

    firstproject

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // mongodb's database user and its' password were used
+    MongoURI: 'mongodb://IPlabuser2:firstproject1@iplab2020-shard-00-00-wmmqs.mongodb.net:27017,iplab2020-shard-00-01-wmmqs.mongodb.net:27017,iplab2020-shard-00-02-wmmqs.mongodb.net:27017/test?ssl=true&replicaSet=IPlab2020-shard-0&authSource=admin&retryWrites=true&w=majority'
+    
+}
